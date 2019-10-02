dec8cc2e07d7296826e87f8608eccdc9f3bf8ec4
Quentin <Coulis1990@gmail.com>
2019-10-02 10:26:27 +0200

    :sparkles: First working iteration

--- main.py
+++ main.py
@@ -84,56 +84,58 @@ def replace_colours(img_lab_data, colours_lab, replaced_colours):
 def colour_to_str(colour):
     return ','.join([str(x) for x in colour])
 
-def merge_colours(img, final_colour_number, colours_lab):
+def merge_colours(img_rgb, final_colour_number, all_colours_rgb):
     final_colours = []
-    for i in range(final_colour_number):
-        idx = [x[0] for x in colours_lab].index(max([x[0] for x in colours_lab]))
-        final_colours.append(colours_lab[idx][1])
-        colours_lab[idx][0] = -1
 
-    final_img = img.copy()
+    for i in range(final_colour_number):
+        idx = [x[0] for x in all_colours_rgb].index(max([x[0] for x in all_colours_rgb]))
+        final_colours.append(all_colours_rgb[idx][1])
+        all_colours_rgb[idx][0] = -1
+
+    final_img = img_rgb.copy()
+
+    #TODO: check if the RGB2LAB conversion MUST occurs at the very last
+    # or if the problems I had were because of code problem uh
 
     for i in range(final_img.shape[0]):
         for j in range(final_img.shape[1]):
-            distances = [dst(final_img[i, j], x) for x in final_colours]
-            print(distances)
+            c1 = color.rgb2lab([[final_img[i, j]]])[0][0]
+            c2 = [color.rgb2lab([[x]])[0][0] for x in final_colours]
+            distances = [dst(c1, x) for x in c2]
             final_img[i, j] = final_colours[distances.index(min(distances))]
 
     return final_img
 
 def dst(c1, c2):
-    return sqrt(pow(c2[0] + c1[0], 2) + pow(c2[1] + c1[1], 2) + pow(c2[2] + c1[2], 2))
+    return sqrt(pow(c2[0] - c1[0], 2) + pow(c2[1] - c1[1], 2) + pow(c2[2] - c1[2], 2))
 
 def main():
     final_colour_number = 4
     # img = cv2.imread('tortank.png')
     img = Image.open('tortank.png').convert('RGB')
 
-    # Convert to Lab colourspace
-    srgb_p = ImageCms.createProfile("sRGB")
-    lab_p  = ImageCms.createProfile("LAB")
-
-    rgb2lab = ImageCms.buildTransformFromOpenProfiles(srgb_p, lab_p, "RGB", "LAB")
-    img_lab = ImageCms.applyTransform(img, rgb2lab)
-
-    img_lab_data = np.asarray(img_lab)
+    # sRGB instead of RGB ([0, 1] instead of [0, 255])
+    img_rgb_data = np.asarray(img) / 255
 
     # Get all the colours in the image
-    colours_lab = img_lab.getcolors()
+    all_colours_rgb = img.getcolors()
+
     # Convert tuples into lists
-    colours_lab = [[x[0], list(x[1])] for x in colours_lab]
+    all_colours_rgb = [[x[0], np.asarray(x[1]) / 255] for x in all_colours_rgb]
 
     # NEW METHOD:
     # take the 4 most represented colours
-    # compute closeness to eahc colour, assign the closest one
-    final_img = merge_colours(img_lab_data, final_colour_number, colours_lab)
+    # compute closeness to each colour, assign the closest one
+    final_img = merge_colours(img_rgb_data, final_colour_number, all_colours_rgb)
 
-    final_img = Image.fromarray(final_img, 'LAB')
+    # MUST specify that the datatype should be uint8 (float in the array),
+    # or else it displays a weird image
+    final_img = np.asarray(final_img * 255, 'uint8')
 
-    lab2rgb = ImageCms.buildTransformFromOpenProfiles(lab_p, srgb_p, "LAB", "RGB")
-    final_img = ImageCms.applyTransform(final_img, lab2rgb)
+    final_img = Image.fromarray(final_img, mode='RGB')
+    final_img.show()
+    final_img.save('tortank_reduced.png')
 
-    breakpoint()
     # OLD METHOD
 
     # # k: original value
